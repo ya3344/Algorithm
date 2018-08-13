9c12f62f1cfe3c37ed5d92726bfee23aa43ff22c
Giruda-92 <41687103+Giruda-92@users.noreply.github.com>
2018-08-13 19:02:04 +0900

    Update Codility_Day4.cpp

--- Algorithm_YJ/Day4/Codility_Day4.cpp
+++ Algorithm_YJ/Day4/Codility_Day4.cpp
@@ -57,3 +57,46 @@ int solution(vector<int> &A)
 
 	return maxProfit;
 }
+
+/*
+Count Non Divisible
+*/
+#include <math.h>
+
+vector<int> solution(vector<int> &A)
+{
+	int N = A.size();
+	int max = 2 * A.size();
+
+	vector<int> table;
+	table.resize(max + 1, 0); // 0 ~ 2N
+
+	vector<int> countTable;
+	countTable.resize(max + 1, 0);
+
+	for (int n : A)
+	{
+		countTable[n]++;
+	}
+
+	for (int n : A)
+	{
+		if (countTable[n] > 0)
+		{
+			for (int j = 1; n*j <= max; j++)
+			{
+				table[n*j] += countTable[n];
+			}
+			countTable[n] = 0;
+		}
+	}
+
+	vector<int> result;
+	result.resize(N);
+	for (int i = 0; i<(int)A.size(); i++)
+	{
+		result[i] = N - table[A[i]];
+	}
+
+	return result;
+}
